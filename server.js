b017298b2e590b22977f4741441f6dc7a38b788b
Eiko Ujifusa <eikorouji@gmail.com>
2022-01-15 23:07:24 -0800

    Added a feature to search one specific animal vs many

--- server.js
+++ server.js
@@ -50,7 +50,10 @@ function filterByQuery(query, animalsArray) {
 }
 
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 // data route for animals //
 app.get('/api/animals', (req, res) => {
@@ -61,6 +64,17 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 
 });
+
+// data route for required parameters for animal array // 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
