e6e2b578f7420262c762f18ab12f93f55a7a9ac1
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-29 15:13:32 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -8,13 +8,62 @@ const { animals } = require ('./data/animals');
 // instantiate the server //
 const app = express();
 
+// new function //
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+
+
+   // animalsArray as filteredResults here:
+    let filteredResults = animalsArray;
+
+    
+
+    // loop through each trait in the personalityTraits array //
+    personalityTraitsArray.forEach(trait => {
+
+        /*// Check the trait against each animal in the filteredResults array.
+      // Remember, it is initially a copy of the animalsArray,
+      // but here we're updating it for each trait in the .forEach() loop.
+      // For each trait being targeted by the filter, the filteredResults
+      // array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one 
+      // of the traits when the .forEach() loop is finished.*/
+
+    filteredResults = filteredResults.filter(
+    animal => animal.personalityTraits.indexOf(trait) !=== -1
+);
+});
+
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    } 
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+
+
+
+
 
 // add route //
 app.get('/api/animals', (req,res) => {
-    res.send('Hello!');
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 // port // server is 3003 because palindome numbers //
 app.listen(3003, () => {
     console.log(`API server now on port 3003!`);
 });
+
+// 11.1.5 get(), send(), res //
